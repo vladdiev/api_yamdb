953737359450dff24e5f1e687683527a748867e7
vladdiev <vladdie@email.com>
2022-09-10 12:45:10 +0300

    implement singnup and get_token endpoints

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,10 +1,14 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from reviews.models import User
 from .permissions import IsAdminOrStaffPermission, IsUserForSelfPermission
-from .serializers import UserSerializer
+from .serializers import (UserSerializer, AuthTokenSerializer,
+                          AuthSignUpSerializer)
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -28,3 +32,51 @@ class UserViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
         serializer = UserSerializer(request.user)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def signup_new_user(request):
+    username = request.data.get('username')
+    if not User.objects.filter(username=username).exists():
+        serializer = AuthSignUpSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        if serializer.validated_data['username'] != 'me':
+            serializer.save()
+            # send_confirmation_code_to_email(username)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(
+            'Username указан неверно!', status=status.HTTP_400_BAD_REQUEST
+        )
+    user = get_object_or_404(User, username=username)
+    serializer = AuthSignUpSerializer(
+        user, data=request.data, partial=True
+    )
+    serializer.is_valid(raise_exception=True)
+    if serializer.validated_data['email'] == user.email:
+        serializer.save()
+        # send_confirmation_code_to_email(username)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(
+        'Почта указана неверно!', status=status.HTTP_400_BAD_REQUEST
+    )
+
+
+@api_view(['POST'])
+def get_token(request):
+    serializer = AuthTokenSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    username = serializer.validated_data['username']
+    confirmation_code = serializer.validated_data['confirmation_code']
+    try:
+        user = User.objects.get(username=username)
+    except ObjectDoesNotExist:
+        return Response(
+            'Пользователь не найден', status=status.HTTP_404_NOT_FOUND
+        )
+    if user.confirmation_code == confirmation_code:
+        refresh = RefreshToken.for_user(user)
+        token_data = {'token': str(refresh.access_token)}
+        return Response(token_data, status=status.HTTP_200_OK)
+    return Response(
+        'Код подтверждения неверный', status=status.HTTP_400_BAD_REQUEST
+    )
