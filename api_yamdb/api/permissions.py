a4eda3753dfcc299f13bb0dd2f7779b8f2cd2956
vladdiev <vladdie@email.com>
2022-09-14 13:27:54 +0300

    fix: pep8

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -6,18 +6,18 @@ class AdminOrReadOnly(BasePermission):
 
     def has_permission(self, request, view):
         return (
-                request.method in SAFE_METHODS
-                or (
-                        request.user.is_authenticated
-                        and request.user.is_admin)
+            request.method in SAFE_METHODS
+            or (
+                request.user.is_authenticated
+                and request.user.is_admin)
         )
 
     def has_object_permission(self, request, view, obj):
         return (
-                request.method in SAFE_METHODS
-                or (
-                        request.user.is_authenticated
-                        and request.user.is_admin)
+            request.method in SAFE_METHODS
+            or (
+                request.user.is_authenticated
+                and request.user.is_admin)
         )
 
 
@@ -31,8 +31,8 @@ class IsAdminOrStaffPermission(BasePermission):
 
     def has_permission(self, request, view):
         return (request.user.is_staff or (
-                request.user.is_authenticated and
-                request.user.is_admin)
+                request.user.is_authenticated
+                and request.user.is_admin)
                 )
 
 
@@ -40,10 +40,10 @@ class IsAuthorOrModerPermission(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (
-                request.method in SAFE_METHODS
-                or obj.author == request.user
-                or (request.user.is_authenticated
-                    and (request.user.is_admin
-                         or request.user.is_moderator)
-                    )
+            request.method in SAFE_METHODS
+            or obj.author == request.user
+            or (request.user.is_authenticated
+                and (request.user.is_admin
+                     or request.user.is_moderator)
+                )
         )
