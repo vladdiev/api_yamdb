267643ef5d792e651f27af0e4474b9326917e5e8
vladdiev <vladdie@email.com>
2022-09-16 19:02:03 +0300

    fix: delete unused import in views

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -9,7 +9,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from reviews.models import Category, Comment, Genre, Review, Title, User
+from reviews.models import Category, Genre, Review, Title, User
 from .filters import TitleFilter
 from .mixins import CreateListDestroyViewSet
 from .permissions import (AdminOrReadOnly, IsAdminOrStaffPermission,
